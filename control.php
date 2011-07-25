89982affb01b3da82aeac267d3c49b1e41df10bc
josh <josh@webeight.net>
2011-07-25 14:55:35 -0400

    created error reporting functions within the db class.

--- control.php
+++ control.php
@@ -1,9 +1,9 @@
 <?
 
 require_once("shows.php");
-$logged_in = $_SESSION['logged_in'];
 $action = isset($_POST['action']) ? $_POST['action'] : false;
 $shows = new Shows();
+$logged_in = $shows->checkLoggedIn();
 
 function ticketDisplay($tickets) {
 	if (substr($tickets, 0 , 7) == 'http://') {
@@ -13,8 +13,7 @@ function ticketDisplay($tickets) {
 }
 
 if ($action == 'next_show') {
-        $nextShow = $shows->nextShow();
-	if ($nextShow) {
+	if ($nextShow = $shows->nextShow()) {
 		echo date('M j, Y', strtotime($nextShow['date'])) . " @ " . $nextShow['venue'] . " in " . $nextShow['city'];
 	} else {		
 		echo "UNAVAILABLE";
@@ -53,16 +52,7 @@ if ($action == 'edit' && $logged_in) {
 	if($tickets == "http://") $tickets = "";
 	$gig = new Gig($date, $city, $venue, $info, $tickets, $id);
 	$shows->editGig($gig);
-        $a = array();
-	if ($_SESSION['error']) {
-		$a['success'] = false;
-		$a['error'] = $_SESSION['msg'];
-		$_SESSION['error'] = false;
-		$_SESSION['msg'] = "";
-	} else {
-		$a['success'] = true;
-	}
-	echo json_encode($a);
+	echo json_encode($shows->checkError());
 }
 
 if ($action == 'add' && $logged_in) {
@@ -74,41 +64,21 @@ if ($action == 'add' && $logged_in) {
 	if($tickets == "http://") $tickets = "";
 	$gig = new Gig($date, $city, $venue, $info, $tickets);
 	$shows->addGig($gig);
-        if ($_SESSION['error']) {
-            $a['success'] = false;
-            $a['error'] = $_SESSION['msg'];
-            $_SESSION['error'] = false;
-            $_SESSION['msg'] = "";
-        } else {
-            $a['success'] = true;
-        }
-        echo json_encode($a);
+        $a = array();
+	echo json_encode($shows->checkError());
 }
 
 if ($action == 'delete' && $logged_in) {
 	$shows->removeGig($_POST['id']);
-        if ($_SESSION['error']) {
-            $a['success'] = false;
-            $a['error'] = $_SESSION['msg'];
-            $_SESSION['error'] = false;
-            $_SESSION['msg'] = "";
-        } else {
-            $a['success'] = true;
-        }
-        echo json_encode($a);
+        echo json_encode($shows->checkError());
 }
 
 if ($action == 'showsTable') {
-    //$gigs = $logged_in ? $shows->listAll() : $shows->listUpcoming();
-	$gigs = $shows->listAll();
-    if ($_SESSION['error']) {
-        $aReturn['success'] = false;
-        $aReturn['error'] = $_SESSION['msg'];
-        $_SESSION['error'] = false;
-        $_SESSION['msg'] = "";
-    } else {
+    if($gigs = $shows->listAll()) {
         if (count($gigs) > 0) {
-            foreach($gigs as $gig) {    
+            $aReturn = array();
+            foreach($gigs as $gig) {
+                $a = array();
                 $a['id'] = $gig->getId();
                 $a['date'] = date('n/j/Y', strtotime($gig->date));
                 $a['time'] = date('g:i a', strtotime($gig->date));
@@ -118,42 +88,11 @@ if ($action == 'showsTable') {
                 $a['tickets'] = ticketDisplay(html_entity_decode($gig->tickets));
                 $aReturn['rows'][] = $a;
             }
+            $aReturn['logged_in'] = $logged_in;
+            $aReturn['success'] = true;
+            echo json_encode($aReturn);
         } else $aReturn['rows'] = false;
-        $aReturn['logged_in'] = $logged_in;
-        $aReturn['success'] = true;
-    }
-    echo json_encode($aReturn);
-}
-
-if ($action == 'displayShows') {
-    $gigs = $shows->listUpcoming();
-    if ($_SESSION['error']) {
-        $aReturn['success'] = false;
-        $aReturn['error'] = $_SESSION['msg'];
-        $_SESSION['error'] = false;
-        $_SESSION['msg'] = "";
-    } else {
-        if (count($gigs) > 0) {
-            $html = "<table id='showsTable' cellpadding='30px'>";
-            foreach($gigs as $gig) {
-                $date = date('n/j/Y', strtotime($gig->date));
-                $time = date('g:i a', strtotime($gig->date));
-                $city = html_entity_decode($gig->city);
-                $venue = html_entity_decode($gig->venue);
-                $info = html_entity_decode($gig->info);
-                $tickets = html_entity_decode($gig->tickets);
-                if (substr($tickets, 0 , 7) == 'http://') {
-                    $tickets = "<a href='$tickets' target='_NEW'><img src='shows/images/tickets_button.gif'></a>";
-                }
-                $html .= "<tr><td>$date<br />$time<br /></td><td>$city<br />$venue</td>"
-                    . "<td>$info</td><td>$tickets</td></tr>";
-            }
-            $html .= "</table>";
-        } else $html = "Currently no upcoming shows.  Check back soon!";
-        $aReturn['success'] = true;
-        $aReturn['msg'] = $html;
-    }
-    echo json_encode($aReturn);
+    } else echo json_encode($shows->getError());
 }
 
 ?>
